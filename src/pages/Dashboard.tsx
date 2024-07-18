6398f8e4cea0130def2b4e844f284919194eb7b5
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2024-07-18 20:35:48 +0500

    add-widget-complete

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,10 +2,11 @@ import { BsSearch } from "react-icons/bs";
 import AdminSidebar from "../components/AdminSidebar";
 import { FaRegBell } from "react-icons/fa";
 import userImg from "../assets/userpic.png";
+import data from "../assets/data.json";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 const dashboard = () => {
   return (
-    <div className="adminContainer">
+    <div className="admin-container">
       <AdminSidebar />
       <main className="dashboard">
         <div className="bar">
@@ -14,7 +15,7 @@ const dashboard = () => {
           <FaRegBell />
           <img src={userImg} alt="User" />
         </div>
-        <section className="widgetcontainer">
+        <section className="widget-container">
           <WidtetItem
             percent={40}
             amount={true}
@@ -22,6 +23,43 @@ const dashboard = () => {
             heading="Revenue"
             color="rgb(0,115,255)"
           />
+          <WidtetItem
+            percent={-14}
+            value={400}
+            heading="Users"
+            color="rgb(0 198 202 )"
+          />
+          <WidtetItem
+            percent={80}
+            value={23000}
+            heading="Transactions"
+            color="rgb(255 196 0)"
+          />
+          <WidtetItem
+            percent={30}
+            value={1000}
+            heading="Products"
+            color="rgb(76 0 25)"
+          />
+        </section>
+        <section className="graph-container">
+          <div className="revenue-chart">
+            <h2>Revenue & Transaction</h2>
+            {/* Graph here */}
+          </div>
+          <div className="dashboard-categories">
+            <h2>Inventory</h2>
+            <div>
+              {data.categories.map((i) => (
+                <CategoryItem
+                  key={i.heading}
+                  heading={i.heading}
+                  value={i.value}
+                  color={`hsl(${i.value * 4},${i.value}%,50%)`}
+                />
+              ))}
+            </div>
+          </div>
         </section>
       </main>
     </div>
@@ -39,10 +77,10 @@ const WidtetItem = ({
   value,
   percent,
   color,
-  amount,
+  amount = false,
 }: WidgetItemProps) => (
   <article className="widget">
-    <div className="WiidgetInfo">
+    <div className="Widget-info">
       <p>{heading}</p>
       <h4>{amount ? `$${value}` : value}</h4>
       {percent > 0 ? (
@@ -55,7 +93,36 @@ const WidtetItem = ({
         </span>
       )}
     </div>
+    <div
+      className="widget-circle"
+      style={{
+        background: ` conic-gradient( 
+     ${color} ${(Math.abs(percent) / 100) * 360}deg,  rgb(255, 255, 255) 0`,
+      }}
+    >
+      <span style={{ color }}>{percent}%</span>
+    </div>
   </article>
 );
 
+interface CategoryItemProps {
+  color: string;
+  value: number;
+  heading: string;
+}
+
+const CategoryItem = ({ color, value, heading }: CategoryItemProps) => (
+  <div className="category-item">
+    <h5>{heading}</h5>
+    <div>
+      <div
+        style={{
+          backgroundColor: color,
+          width: `${value}%`,
+        }}
+      ></div>
+    </div>
+    <span>{value}%</span>
+  </div>
+);
 export default dashboard;
