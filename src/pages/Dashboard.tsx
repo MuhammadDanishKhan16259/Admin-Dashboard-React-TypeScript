c614f72cc449dd1087a75ab2def94cdb48d7b1fb
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2024-07-18 19:04:03 +0500

    add-widget

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,61 @@
+import { BsSearch } from "react-icons/bs";
 import AdminSidebar from "../components/AdminSidebar";
-
+import { FaRegBell } from "react-icons/fa";
+import userImg from "../assets/userpic.png";
+import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 const dashboard = () => {
   return (
     <div className="adminContainer">
       <AdminSidebar />
-      <main>assdsa</main>
+      <main className="dashboard">
+        <div className="bar">
+          <BsSearch />
+          <input type="text" placeholder="Search for data, users, docs" />
+          <FaRegBell />
+          <img src={userImg} alt="User" />
+        </div>
+        <section className="widgetcontainer">
+          <WidtetItem
+            percent={40}
+            amount={true}
+            value={340000}
+            heading="Revenue"
+            color="rgb(0,115,255)"
+          />
+        </section>
+      </main>
     </div>
   );
 };
+interface WidgetItemProps {
+  heading: string;
+  value: number;
+  percent: number;
+  color: string;
+  amount?: boolean;
+}
+const WidtetItem = ({
+  heading,
+  value,
+  percent,
+  color,
+  amount,
+}: WidgetItemProps) => (
+  <article className="widget">
+    <div className="WiidgetInfo">
+      <p>{heading}</p>
+      <h4>{amount ? `$${value}` : value}</h4>
+      {percent > 0 ? (
+        <span className="green">
+          <HiTrendingUp /> + {percent}%
+        </span>
+      ) : (
+        <span className="red">
+          <HiTrendingDown /> {percent}%
+        </span>
+      )}
+    </div>
+  </article>
+);
 
 export default dashboard;
